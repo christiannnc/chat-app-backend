75800b1f4780bf714ff12fd998a0be33132fba7e
christiannnc <c917unc@gmail.com>
2022-11-28 08:14:04 -0500

    layed out the foundation for the client-server connection via socketio

--- app.js
+++ app.js
@@ -3,5 +3,30 @@ const app = express();
 
 const port = 3001;
 
+const io = require("socket.io")(3002, {
+    cors: { origin: "http://localhost:3000" },
+});
+
+io.on("connection", (socket) => {
+    console.log(`new user: ${socket.id}`);
+    socket.on("join-room", (room) => {
+        socket.join(room);
+        console.log(`user joined room ${room}`);
+    });
+
+    socket.on("send-message", (message, room, senderId) => {
+        console.log(`message: ${message} room: ${room} id: ${senderId}`);
+        socket.to(room).emit("incoming-message", {
+            message,
+            senderId,
+        });
+    });
+
+    socket.on("ping", (room) => {
+        console.log("ping! to room " + room);
+        io.to(room).emit("pong", "pong!");
+    });
+});
+
 app.listen(port);
 console.log(`Listening on port ${port} 🚀`);
